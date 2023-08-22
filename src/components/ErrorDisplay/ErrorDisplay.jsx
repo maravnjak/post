cf89c09bcc3b41afee373f044760744f7ce74da9
Marijana Ravnjak <marijana@24setup.com>
2023-08-22 10:24:17 +0200

    feat(#23): Create component Error Display with witch displayed success message deleted user

--- src/components/ErrorDisplay/ErrorDisplay.jsx
+++ src/components/ErrorDisplay/ErrorDisplay.jsx
@@ -1,23 +1,20 @@
 import React from 'react'
-import { Alert, IconButton } from '@mui/material'
+import { Typography } from '@mui/material'
 import PropTypes from 'prop-types'
 import { useTranslation } from 'common/i18n'
 
-const ErrorDisplay = ({ handleOpen, handleSuccess, displayError }) => {
+const ErrorDisplay = ({ displayError }) => {
   const { t } = useTranslation()
 
-  console.log('error display, error ', displayError)
 
   return (
-    <IconButton handleOpen={handleOpen}>
-      <Alert severity="success" handleSuccess={handleSuccess}>{displayError}</Alert>
-    </IconButton>
-  )
 
+    <Typography>
+      {displayError}
+    </Typography>
+  )
 }
 ErrorDisplay.propTypes = {
-  handleSuccess: PropTypes.func.isRequired,
-  handleOpen: PropTypes.func.isRequired,
   displayError: PropTypes.string.isRequired
 }
 export default ErrorDisplay
